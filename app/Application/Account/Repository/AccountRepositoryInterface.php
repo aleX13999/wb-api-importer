c08f5d1376ece62d20aa910d4991de8eb5a23a67
aleX13999 <leaman00@mail.ru>
2025-09-29 22:56:28 +0700

    Добавил возможность указать accountId при загрузке данных

--- app/Application/Account/Repository/AccountRepositoryInterface.php
+++ app/Application/Account/Repository/AccountRepositoryInterface.php
@@ -3,9 +3,11 @@
 namespace App\Application\Account\Repository;
 
 use App\Models\Account;
+use Illuminate\Database\Eloquent\Collection;
 
 interface AccountRepositoryInterface
 {
     public function getOne(int $id): ?Account;
     public function getOneByName(string $name): ?Account;
+    public function getAll(): Collection;
 }
--- app/Console/Commands/ImportDataCommand.php
+++ app/Console/Commands/ImportDataCommand.php
@@ -2,29 +2,98 @@
 
 namespace App\Console\Commands;
 
-use App\Jobs\ImportIncomesJob;
-use App\Jobs\ImportOrdersJob;
-use App\Jobs\ImportSalesJob;
-use App\Jobs\ImportStocksJob;
+use App\Application\Account\Repository\AccountRepositoryInterface;
+use App\Models\Income;
+use App\Models\Order;
+use App\Models\Sale;
+use App\Models\Stock;
+use App\Services\LoadDataService;
+use DateTime;
 use Illuminate\Console\Command;
 
 class ImportDataCommand extends Command
 {
-    protected $signature = 'import:data';
+    protected $signature = 'import:data {--accountId=}';
 
     protected $description = 'Импортирует данные из API и сохраняет в базу';
 
+    public function __construct(
+        private readonly AccountRepositoryInterface $accountRepository,
+        private readonly LoadDataService            $loadDataService,
+    ) {
+        parent::__construct();
+    }
+
     public function handle(): void
     {
+
+        $this->info('Импорт данных начался');
+
+        $accountId = $this->option('accountId');
+        if ($accountId) {
+            $accounts = [$this->accountRepository->getOne($accountId)];
+        } else {
+            $accounts = $this->accountRepository->getAll();
+        }
+
         try {
-            ImportSalesJob::dispatch();
-            ImportOrdersJob::dispatch();
-            ImportStocksJob::dispatch();
-            ImportIncomesJob::dispatch();
+            foreach ($accounts as $account) {
+                $dateFrom = '1900-01-01';
+                $dateTo = new DateTime('today');
+
+                $this->loadDataService->loadAllData(
+                    endpoint: 'sales',
+                    query:    ['dateFrom' => $dateFrom, 'dateTo' => $dateTo->format('Y-m-d')],
+                    saveCallback: function (array $items) {
+                        foreach ($items as $item) {
+                            Sale::create(
+                                $item,
+                            );
+                        }
+                    },
+                );
+
+                $this->loadDataService->loadAllData(
+                    endpoint: 'orders',
+                    query:    ['dateFrom' => $dateFrom, 'dateTo' => $dateTo->format('Y-m-d')],
+                    saveCallback: function (array $items) {
+                        foreach ($items as $item) {
+                            Order::create(
+                                $item,
+                            );
+                        }
+                    },
+                );
+
+                $dateFrom = new DateTime('today');
+                $this->loadDataService->loadAllData(
+                    endpoint: 'stocks',
+                    query:    ['dateFrom' => $dateFrom->format('Y-m-d')],
+                    saveCallback: function (array $items) {
+                        foreach ($items as $item) {
+                            Stock::create(
+                                $item,
+                            );
+                        }
+                    },
+                );
+
+                $this->loadDataService->loadAllData(
+                    endpoint: 'incomes',
+                    query:    ['dateFrom' => $dateFrom, 'dateTo' => $dateTo->format('Y-m-d')],
+                    saveCallback: function (array $items) {
+                        foreach ($items as $item) {
+                            Income::create(
+                                $item,
+                            );
+                        }
+                    },
+                );
+            }
         } catch (\Exception $e) {
             $this->error($e->getMessage());
         }
 
-        $this->info('Импорт данных запущен');
+        $this->info('Импорт данных закончен');
     }
 }
